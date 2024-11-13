4393ca6e922c183759e959509c4a918d8c0286a5
loyiwen <lo.yiwen@proton.me>
2024-11-13 04:20:34 +0000

    chore: reformatting brackets

--- my_shell.c
+++ my_shell.c
@@ -3,24 +3,20 @@
 #include "kernel/fcntl.h"
 
 /* Read a line of characters from stdin. */
-int getcmd(char *buf, int nbuf)
-{
+int getcmd(char *buf, int nbuf) {
 	printf(">>> "); // Display prompt
 	memset(buf, 0, nbuf);
 	gets(buf, nbuf);
 
-	if (buf[0] == 0) // EOF
+	if (buf[0] == 0) { // EOF
 		return -1;
-	
+	}
+
 	return 0;
 }
 
-/*
-	A recursive function which parses the command at *buf and executes it.
-*/
-__attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
-{
-
+/* A recursive function which parses the command at *buf and executes it. */
+__attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp) {
 	/* Useful data structures and flags. */
 	char *arguments[10];
 	int numargs = 0;
@@ -36,17 +32,14 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 	int sequence_cmd = 0;
 
 	/* Parse the command character by character. */
-	for (int i = 0; i < nbuf; i++)
-	{
+	for (int i = 0; i < nbuf; i++) {
 
 		/* Parse the current character and set-up various flags: sequence_cmd, redirection, pipe_cmd and similar. */
 		/* ##### Place your code here. */
 
 		// Handle spaces, newlines, tabs, and end of string
-		if (buf[i] == ' ' || buf[i] == '\n' || buf[i] == '\t' || buf[i] == '\0')
-		{
-			if (we)
-			{
+		if (buf[i] == ' ' || buf[i] == '\n' || buf[i] == '\t' || buf[i] == '\0') {
+			if (we)	{
 				buf[i] = '\0'; // Terminate the current word
 				we = 0; // Set flag to indicate that buf[i] is no longer inside a word
 			}
@@ -54,49 +47,38 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 			continue;
 		}
 
-		if (buf[i] == '<' && !redirection_flag)
-		{
+		if (buf[i] == '<' && !redirection_flag)	{
 			
 			buf[i] = '\0'; // Terminates previous argument
 			redirection_left = 1;
 			redirection_flag = 1;
 			i++;
-		}
-		else if (buf[i] == '>' && !redirection_flag)
-		{
+		} else if (buf[i] == '>' && !redirection_flag) {
 			buf[i] = '\0'; // Terminates previous argument
 			redirection_right = 1;
 			redirection_flag = 1;
 			i++;
 		}
 
-		if (!(redirection_left || redirection_right))
-		{
+		if (!(redirection_left || redirection_right)) {
 			/* No redirection, continue parsing command. */
 			// #### Place your code here.
-		}
-		else
-		{
+		} else {
 			/* Redirection command. Capture the file names. */
 			// ##### Place your code here.
 			while (buf[i] == ' ' || buf[i] == '\t') i++; // Skip spaces after '<' or '>'
 
-			if (redirection_left)
-			{
+			if (redirection_left) {
 				file_name_l = &buf[i];
-			}
-			else
-			{
+			} else {
 				file_name_r = &buf[i];
 			}
 			while (buf[i] != ' ' && buf[i] != '\t' && buf[i] != '\n' && buf[i] != '\0') i++;
     		buf[i] = '\0'; // Null-terminate the filename
     		continue;
-
 		}
 
-		if (ws)
-		{
+		if (ws) {
 			arguments[numargs++] = &buf[i]; // Store start of new word
 			ws = 0;
 			we = 1;
@@ -109,11 +91,9 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 	/*
 	  Sequence command. Continue this command in a new process. Wait for it to complete and execute the command following ';'.
 	*/
-	if (sequence_cmd)
-	{
+	if (sequence_cmd) {
 		sequence_cmd = 0;
-		if (fork() != 0)
-		{
+		if (fork() != 0) {
 			wait(0);
 			// ##### Place your code here.
 		}
@@ -123,11 +103,9 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 	/*
 	  If this is a redirection command, tie the specified files to std in/out.
 	*/
-	if (redirection_left)
-	{
+	if (redirection_left) {
 		// ##### Place your code here.
-		if (file_name_l)
-		{
+		if (file_name_l) {
 			int fd_in = open(file_name_l, O_RDONLY);
 			if (fd_in < 0) {
 				fprintf(2, "Error: cannot open file %s for reading\n", file_name_l);
@@ -135,25 +113,21 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 			}
 
 			close(0); // Close stdin
-			if (dup(fd_in) != 0)
-			{
+			if (dup(fd_in) != 0) {
 				fprintf(2, "Error: duplication failed for input redirection\n");
 				close(fd_in);
 				exit(1);
 			}
 			close(fd_in);
-		}
-		else
-		{
+		} else {
 			fprintf(2, "Error: missing filename after '<'\n");
         	exit(1);
 		}
 	}
-	if (redirection_right)
-	{
+	
+	if (redirection_right) {
 		// ##### Place your code here.
-		if (file_name_r)
-		{
+		if (file_name_r) {
 			int fd_out = open(file_name_r, O_WRONLY | O_CREATE | O_TRUNC);
 			if (fd_out < 0) {
 				fprintf(2, "Error: cannot open file %s for writing\n", file_name_r);
@@ -161,16 +135,13 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 			}
 
 			close(1); // Close stdout
-			if (dup(fd_out) != 1)
-			{
+			if (dup(fd_out) != 1) {
 				fprintf(2, "Error: duplication failed for output redirection\n");
 				close(fd_out);
 				exit(1);
 			}
 			close(fd_out);
-		}
-		else
-		{
+		} else {
 			fprintf(2, "Error: missing filename after '<'\n");
         	exit(1);
 		}
@@ -183,82 +154,62 @@ __attribute__((noreturn)) void run_command(char *buf, int nbuf, int *pcp)
 	/*
 		If this command is 'cd', write the arguments to the pcp pipe and exit with '2' to tell the parent process about this.
 	*/
-	if (strcmp(arguments[0], "cd") == 0)
-	{
+	if (strcmp(arguments[0], "cd") == 0) {
 		// ##### Place your code here.
-		if (numargs < 2)
-		{
+		if (numargs < 2) {
 			printf("cd: missing argument\n");
-		}
-		else if (chdir(arguments[1]) < 0)
-		{
+		} else if (chdir(arguments[1]) < 0) {
 			printf("cd: cannot change directory to %s\n", arguments[1]);
 		}
 		write(pcp[1], arguments[1], strlen(arguments[1]) + 1); // Notify the parent process
 		exit(2); // Exit with status 2 to indicate a cd command
-	}
-	else
-	{
+	} else {
 		/*
 		  Pipe command: fork twice. Execute the left hand side directly. Call run_command recursion for the right side of the pipe.
 		*/
-		if (pipe_cmd)
-		{
+		if (pipe_cmd) {
 			// ##### Place your code here.
-			if (fork() == 0)
-			{
+			if (fork() == 0) {
 				exec(arguments[0], arguments);
 				fprintf(2, "exec %s failed\n", arguments[0]);
 				exit(1);
-			}
-			else
-			{
+			} else {
 				wait(0); // Wait for the child process to finish
 			}
-			
-		}
-		else
-		{
+		} else {
 			// ##### Place your code here.
 			// Handle regular commands without pipes
-			if (fork() < 0)
-			{
+			if (fork() < 0)	{
 				fprintf(2, "fork failed\n");
 				exit(1);
 			}
-			if (fork() == 0) // Child process
-			{
+
+			if (fork() == 0) { // Child process
 				exec(arguments[0], arguments);
 				fprintf(2, "exec %s failed\n", arguments[0]);
 				exit(1);
-			}
-			else // Parent process
-			{ 
+			} else { // Parent process
 				wait(0); // Wait for the child process to finish
 			}
 		}
 	}
 
 	// If no command entered, return
-	if (numargs == 0)
-	{
+	if (numargs == 0) {
 		exit(0);
 	}
 	exit(0);
 }
 
 
-int main(void)
-{
+int main(void) {
 	static char buf[100];
 	int pcp[2];
 	pipe(pcp);
 
 	/* Read and run input commands. */
-	while (getcmd(buf, sizeof(buf)) >= 0)
-	{
-		if (fork() == 0)
-		{
+	while (getcmd(buf, sizeof(buf)) >= 0) {
+		if (fork() == 0) {
 			run_command(buf, 100, pcp);
 		}
 		/*
@@ -266,14 +217,11 @@ int main(void)
 		*/
 		int child_status;
 		// ##### Place your code here
-		if (wait(&child_status) >= 0)
-		{
-			if (child_status == 2) // If exit status is 2, it's a 'cd' command
-			{
+		if (wait(&child_status) >= 0) {
+			if (child_status == 2) { // If exit status is 2, it's a 'cd' command
                 char new_dir[100];
                 read(pcp[0], new_dir, sizeof(new_dir)); // Read the directory from the pipe
-                if (chdir(new_dir) < 0)
-				{
+                if (chdir(new_dir) < 0) {
                     printf("cd: cannot change directory to %s\n", new_dir);
                 }
             }
